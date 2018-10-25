ae97c4401fb44dc3b057a9e262aab36fa096274f
Yoram <soussan.yoram@gmail.com>
2018-10-25 13:50:58 +0200

    Knapsack algorithm

--- permute.py
+++ permute.py
@@ -41,6 +41,16 @@ def fibonnacci(n):
     return x
 print(fibonacci(100))
 
+#### Knapsack ####
+
+def sacados(liste, i):
+    if len(liste) == 0:
+        return i == 0
+    else:
+        return (sacados(liste[1:], i)
+                or sacados(liste[1:], i - liste[0]))
+print(sacados([2, 7, -1, 9], 16))
+print(sacados([2, 7, -1, 9], 4))
 
 
 
